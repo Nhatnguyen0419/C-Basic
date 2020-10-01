c61a271035442b6af8a11cd845ef3981813b8f9a
fncng193 <nhatnguyen0419@gmail.com>
2020-10-01 22:29:38 +0700

    delegate& fileStream

---
+++ C_Sharp_Basic/Delegate/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        public delegate void PriceChangeHandler(decimal oldPrice, decimal newPrice);
+        public class Product
+        {
+            private decimal _price;
+            public event PriceChangeHandler PriceChanged;
+            public decimal Price
+            {
+                get { return _price; }
+
+
+                set
+                {
+                    if (_price == value)
+                    {
+                        return;
+                    }
+                    decimal oldPrice = _price;
+                    _price = value;
+                    if (PriceChanged != null)
+                    {
+                        PriceChanged(oldPrice, _price);
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Price = 1500;
+            product.PriceChanged += product_PriceChanged;
+            product.Price = 3000;
+            Console.ReadKey();
+        }
+        public static void product_PriceChanged(decimal oldPrice,decimal newPrice)
+        {
+            Console.WriteLine("Old price: {0}", oldPrice);
+            Console.WriteLine("New price: {0}", newPrice);
+        }
+    }
+}
---
+++ C_Sharp_Basic/FileStreamEx/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace FileStreamEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fileStream = new FileStream("text.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            for (int i = 1; i <= 20; i++)
+            {
+                fileStream.WriteByte((byte)i);
+            }
+            fileStream.Position = 0;
+            Console.WriteLine("Write Ok.");
+            for(int i = 0; i <= 20; i++)
+            {
+                Console.WriteLine(fileStream.ReadByte() + "\t");
+            }
+            fileStream.Close();
+            Console.ReadKey();
+
+        }
+    }
+}
